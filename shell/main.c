61dd69ae84eeb1f70b867313e00d6c0fee0c2220
adam <apk@pluvi.us>
2017-10-09 14:44:50 -0400

    add gitignore and move cd to main file

--- shell/main.c
+++ shell/main.c
@@ -1,9 +1,39 @@
+#include <string.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+
 #include "main.h"
+
+/* changedir moves cwd from cwd to dir,
+ * or $HOME if NULL.
+ * return 0 == success
+ * return -1 == failure
+ */
+int
+changedir(char *dir) {
+	if (chdir(dir) == -1) {
+		sprintf(stderr, "changedir(): %s\n", strerror(errno));
+		return -1;
+	}
+	return 0;
+}
 
 int
 main(int argc, char **argv) {
-	if(!strcmp(argv[1], "cd"))
-		changedir(argv[2]);
+	char* promptline, curtok, str;
+	char *args[];
+	int count;
+
+	while (1) {
+		promptline = malloc(PROMPTLINE * sizeof(char));
+		printf("% ");
+		fgets(&promptline, PROMPTLINE, stdin);
+
+	}
 
 	return 0;
 }
---
+++ shell/main.h
@@ -0,0 +1,4 @@
+#define PROMPTLINE 65536
+
+int changedir(char *dir);
+void about(void);
